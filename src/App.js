2145cc401b27d15c1a6ce811a677a557f6d106fe
SriSravya28 <vallurisrisravya96@gmail.com>
2025-03-10 02:30:21 +0530

    modified styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import CourseDetail from './pages/CourseDetail';
 import SuccessPage from './pages/SuccessPage';
-import Login from './pages/Login';
 import './App.css';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/course/:id" element={<CourseDetail />} />
           <Route path="/success" element={<SuccessPage />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
