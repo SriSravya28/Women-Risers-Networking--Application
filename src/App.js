0a51c21a8cb7e1461332d77be14c1585158fb634
SriSravya28 <vallurisrisravya96@gmail.com>
2025-03-10 02:29:10 +0530

    styling has reverted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import CourseDetail from './pages/CourseDetail';
 import SuccessPage from './pages/SuccessPage';
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/course/:id" element={<CourseDetail />} />
           <Route path="/success" element={<SuccessPage />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
